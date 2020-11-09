0772a6a957289dd6de963272878a58e777a0d715
devadyuti <devadyuti@yahoo.com>
2020-11-09 02:19:43 +0000

    Issue 1: Create Lambda API for taleofddh core

--- taleofddh-data-core/app.js
+++ taleofddh-data-core/app.js
@@ -1,92 +1,56 @@
 'use strict'
 
 var MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 //uncomment this line when running from AWS with KMS
 //const decrypt = require("./decrypt");
-var core = require('./api');
+
 
 let atlas_connection_uri;
 let cachedDb = null;
 
 exports.handler = (event, context, callback) => {
-    var uri = process.env['MONGODB_ATLAS_CLUSTER_URI'];
-
-    if (atlas_connection_uri != null) {
-        processEvent(event, context, callback);
-    }
-    else {
-        //comment this line when running from AWS with KMS
-        atlas_connection_uri = uri;
-        //uncomment this line when running from AWS with KMS
-        //atlas_connection_uri = await decrypt('MONGODB_ATLAS_CLUSTER_URI');
-        //console.log('the Atlas connection string is ' + atlas_connection_uri);
-        processEvent(event, context, callback);
-    }
+    processEvent(event, context, callback);
 };
 
-const processEvent = (event, context, callback) => {
+const processEvent = async (event, context, callback) => {
     console.log('Calling MongoDB Atlas from AWS Lambda with event: ' + JSON.stringify(event));
-
     let jsonContents = JSON.parse(JSON.stringify(event));
 
-    //date conversion for grades array
-    if(jsonContents.grades != null) {
-        for(let i = 0, len=jsonContents.grades.length; i < len; i++) {
-            //use the following line if you want to preserve the original dates
-            //jsonContents.grades[i].date = new Date(jsonContents.grades[i].date);
+    //the following line is critical for performance reasons to allow re-use of database connections across calls to this Lambda function and avoid closing the database connection. The first call to this lambda function takes about 5 seconds to complete, while subsequent, close calls will only take a few hundred milliseconds.
+    context.callbackWaitsForEmptyEventLoop = false;
 
-            //the following line assigns the current date so we can more easily differentiate between similar records
-            jsonContents.grades[i].date = new Date();
-        }
-    }
+    const db = await connectToDatabase(context);
+    const result = await findDocuments(db, jsonContents, callback);
+}
+
+const connectToDatabase = async (context) => {
+    //comment these lines when running from AWS with KMS
+    var uri = process.env['MONGODB_ATLAS_CLUSTER_URI'];
+    atlas_connection_uri = uri;
+    //uncomment this line when running from AWS with KMS
+    //atlas_connection_uri = await decrypt('MONGODB_ATLAS_CLUSTER_URI');
+    //console.log('the Atlas connection string is ' + atlas_connection_uri);
 
     //the following line is critical for performance reasons to allow re-use of database connections across calls to this Lambda function and avoid closing the database connection. The first call to this lambda function takes about 5 seconds to complete, while subsequent, close calls will only take a few hundred milliseconds.
     context.callbackWaitsForEmptyEventLoop = false;
     var database = process.env['DB_NAME'];
 
-    try {
-        if (cachedDb == null) {
-            console.log('=> connecting to database', database);
-            MongoClient.connect(atlas_connection_uri, function (err, client) {
-                cachedDb = client.db(database);
-                return executeDbQuery(cachedDb, jsonContents, callback)
-            });
-        }  else {
-            executeDbQuery(cachedDb, jsonContents, callback);
-        }
+    if (cachedDb) {
+        return Promise.resolve(cachedDb);
     }
-    catch (err) {
-        console.error('an error occurred', err);
-    }
+    console.log('=> connecting to database', database);
+    const client = await MongoClient.connect(atlas_connection_uri);
+    cachedDb = client.db(database);
+    return cachedDb;
 }
 
-const executeDbQuery = (db, json, callback) => {
-    let result = {};
-    switch(json.action) {
-        case 'findActiveMenuList':
-            result = core.findActiveMenuList(cachedDb, json, callback);
-            break;
-        case 'findActivePromotionList':
-            result = core.findActivePromotionList(cachedDb, json, callback);
-            break;
-        case 'findAllTermsAndConditionsList':
-            result = core.findAllTermsAndConditionsList(cachedDb, json, callback);
-            break;
-        case 'findAllPrivacyPolicyList':
-            result = core.findAllPrivacyPolicyList(cachedDb, json, callback);
-            break;
-        case 'findAllFrequentlyAskedQuestionList':
-            result = core.findAllFrequentlyAskedQuestionList(cachedDb, json, callback);
-            break;
-        case 'findCountryByName':
-            result = core.findCountryByName(cachedDb, json, callback);
-            break;
-        case 'findCountryByCode':
-            result = core.findCountryByCode(cachedDb, json, callback);
-            break;
-        default:
-            result = core.createDoc(cachedDb, json, callback);
-            break;
-    }
-    return result;
+const findDocuments = async (db, json, callback) =>  {
+    const docs = await db.collection(json.event).find(json.filters).toArray();
+    await db.collection(json.event).find(json.filters).toArray(function(err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following documents");
+        console.log(docs);
+        callback(null, docs);
+    });
 }
--- taleofddh-data-core/taleofddh-data-core-run.cmd
+++ taleofddh-data-core/taleofddh-data-core-run.cmd
@@ -13,4 +13,4 @@ REM lambda-local -l app.js -e frequentlyAskedQuestion.json -E {\"MONGODB_ATLAS_C
 
 REM lambda-local -l app.js -e countryName.json -E {\"MONGODB_ATLAS_CLUSTER_URI\":\"%1?retryWrites=true&w=majority\",\"DB_NAME\":\"%2\"}
 
-REM lambda-local -l app.js -e countryCode.json -E {\"MONGODB_ATLAS_CLUSTER_URI\":\"%1?retryWrites=true&w=majority\",\"DB_NAME\":\"%2\"}
+lambda-local -l app.js -e countryCode.json -E {\"MONGODB_ATLAS_CLUSTER_URI\":\"%1?retryWrites=true&w=majority&authSource=admin\",\"DB_NAME\":\"%2\"}
