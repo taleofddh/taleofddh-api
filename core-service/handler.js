c1c4cd59eacbb2114eb383957006a86ce9b141b7
devadyuti <devadyuti@yahoo.com>
2020-11-16 14:02:10 +0000

    Issue 1: Create Lambda API for taleofddh core

---
+++ core-service/handler.js
@@ -0,0 +1,111 @@
+'use strict';
+const db = require('./db');
+const collection = process.env['COLLECTION_NAME'];
+
+module.exports.menuList = async (event) => {
+    let active  = (event.pathParameters.active === 'true');
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {"active" : active});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.promotionList = async (event) => {
+    let active  = (event.pathParameters.active === 'true');
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {"active" : active});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.aboutUsList = async (event) => {
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.termsAndConditionsList = async (event) => {
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.privacyPolicyList = async (event) => {
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.frequentlyAskedQuestionList = async (event) => {
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.countryByCode = async (event) => {
+    let countryCode  = event.pathParameters.countryCode;
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {"code" : countryCode});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
+module.exports.countryByName = async (event) => {
+    let countryName  = event.pathParameters.countryName;
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, {"name" : countryName});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
