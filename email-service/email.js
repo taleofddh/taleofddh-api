fa0b99b608e4aa64e4f248d578ba21b654e8e6ca
devadyuti <devadyuti@yahoo.com>
2020-11-19 23:55:41 +0000

    Issue 4: Create Lambda API for Email Service

---
+++ email-service/email.js
@@ -0,0 +1,36 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'eu-west-1'});
+
+module.exports.send = async (toAddress, templateName, templateData, ccAddress) => {
+    var ccList = ccAddress ? ccAddress : [];
+    // Create sendEmail params
+    var params = {
+        Destination: { /* required */
+            CcAddresses: ccList,
+            ToAddresses: [
+                toAddress
+                /* more items */
+            ]
+        },
+        Source: process.env['FROM_ADDRESS'], /* required */
+        Template: templateName,
+        TemplateData: templateData
+    };
+
+    // Create a SES client
+    var ses = new AWS.SES({apiVersion: '2010-12-01'});
+
+    // for async it only works with Promise and resolve/reject
+    return new Promise((resolve, reject) => {
+        ses.sendTemplatedEmail(params, function(err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
---
+++ email-service/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+const email = require('./email');
+
+module.exports.sendEnquiryMessage = async (event) => {
+    const enquiry = JSON.parse(event.body);
+    const data = JSON.stringify({ "name" : enquiry.name, "subject": enquiry.subject, "number": enquiry.number, "message": enquiry.message });
+    const messageId = await email.send(enquiry.to, process.env['TEMPLATE_NAME'], data);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(messageId),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
