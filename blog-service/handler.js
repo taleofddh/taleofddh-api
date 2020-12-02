f4dac4d0089d5d6e9ef2fa4193428bdbab184dfa
devadyuti <devadyuti@yahoo.com>
2020-12-02 18:41:06 +0000

    Issue 6:Create Blog API using Lambda Functions

--- blog-service/handler.js
+++ blog-service/handler.js
@@ -15,6 +15,21 @@ module.exports.findBlogList = async (event) => {
     };
 };
 
+module.exports.findCategorizedBlogList = async (event) => {
+    const data = JSON.parse(event.body);
+    let homePageFlag = data.homePageBlog === 'true';
+    const database = await db.get();
+    const docs = await db.findDocuments(database, collection, homePageFlag ? {"category": data.category, "homePageFlag": homePageFlag} : {"category": data.category}, {"sequence": 1});
+    return {
+        statusCode: 200,
+        body: JSON.stringify(docs),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+};
+
 module.exports.findArticleList = async (event) => {
     const data = JSON.parse(event.body);
     const database = await db.get();
