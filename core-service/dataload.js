6659ae4dee819695fdd7941e90b96a0be29ac18d
devadyuti <devadyuti@yahoo.com>
2021-01-08 11:41:01 +0000

    Issue 1:Create Lambda API for taleofddh core

--- core-service/dataload.js
+++ core-service/dataload.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
 const dotenv = require('dotenv');
-const MongoClient = require('mongodb').MongoClient;
+const database = require('./db');
 
 dotenv.config();
 
-let menus = [];
+let menuDeleteKeys = [];
+let menuItems = [];
+let menuGetKeys = [];
 fs.createReadStream(path.resolve(__dirname, 'data', 'menu.csv'))
     .pipe(csv.parse({ headers: true }))
     .transform(data => ({
@@ -18,44 +20,90 @@ fs.createReadStream(path.resolve(__dirname, 'data', 'menu.csv'))
         icon: data.icon,
         condition: data.condition,
         active: data.active.toUpperCase() === 'TRUE',
-        position: data.position,
+        position: data.position
     }))
     .on('error', error => console.error(error))
     .on('data', row => {
-        menus.push(row);
+        menuDeleteKeys.push({
+            DeleteRequest: {
+                Key: {
+                    "sequence": row.sequence
+                }
+            }
+        });
+        menuItems.push({
+            PutRequest: {
+                Item: {
+                    "sequence": row.sequence,
+                    "name": row.name,
+                    "type": row.type,
+                    "link": row.link,
+                    "external": row.external,
+                    "icon": row.icon,
+                    "condition": row.condition,
+                    "active": row.active,
+                    "position": row.position
+                }
+            }
+        });
+        menuGetKeys.push({
+            "sequence": row.sequence
+        });
     })
     .on('end', async rowCount => {
-        menus.sort((a,b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
-        await dbOperation("deleteDocs", "menu", [], {});
-        await dbOperation("insertDocs", "menu", menus);
-        const docs = await dbOperation("findDocs", "menu", [], {"active": true}, {"sequence": 1});
+        await dbOperation("deleteDocs", "menu", menuDeleteKeys, {});
+        await dbOperation("insertDocs", "menu", menuItems);
+        const docs = await dbOperation("findDocs", "menu", menuGetKeys);
         await console.log(docs);
         await console.log(`Parsed ${rowCount} rows`);
     });
 
-let aboutUsList = [];
+let aboutUsDeleteKeys = [];
+let aboutUsItems = [];
+let aboutUsGetKeys = [];
 fs.createReadStream(path.resolve(__dirname, 'data', 'aboutUs.csv'))
     .pipe(csv.parse({ headers: true, delimiter: '|' }))
     .transform(data => ({
         sequence: parseInt(data.sequence),
         header: data.header,
         description: data.description,
-        image: data.image,
+        image: data.image
     }))
     .on('error', error => console.error(error))
     .on('data', row => {
-        aboutUsList.push(row)
+        aboutUsDeleteKeys.push({
+            DeleteRequest: {
+                Key: {
+                    "sequence": row.sequence
+                }
+            }
+        });
+        aboutUsItems.push({
+            PutRequest: {
+                Item: {
+                    "sequence": row.sequence,
+                    "header": row.header,
+                    "description": row.description,
+                    "image": row.image
+                }
+            }
+        });
+        aboutUsGetKeys.push({
+            "sequence": row.sequence
+        });
+
     })
     .on('end', async rowCount => {
-        aboutUsList.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
-        await dbOperation("deleteDocs", "aboutUs", [], {});
-        await dbOperation("insertDocs", "aboutUs", aboutUsList);
-        const docs = await dbOperation("findDocs", "aboutUs", [], {}, {"sequence": 1});
+        await dbOperation("deleteDocs", "aboutUs", aboutUsDeleteKeys, {});
+        await dbOperation("insertDocs", "aboutUs", aboutUsItems);
+        const docs = await dbOperation("findDocs", "aboutUs", aboutUsGetKeys);
         await console.log(docs);
         await console.log(`Parsed ${rowCount} rows`);
     });
 
-let promotions = [];
+let promotionDeleteKeys = [];
+let promotionItems = [];
+let promotionGetKeys = [];
 fs.createReadStream(path.resolve(__dirname, 'data', 'promotion.csv'))
     .pipe(csv.parse({ headers: true }))
     .transform(data => ({
@@ -64,80 +112,266 @@ fs.createReadStream(path.resolve(__dirname, 'data', 'promotion.csv'))
         tagLine: data.tagLine,
         image: data.image,
         link: data.link,
-        active: data.active.toUpperCase() === 'TRUE',
+        active: data.active.toUpperCase() === 'TRUE'
     }))
     .on('error', error => console.error(error))
     .on('data', row => {
-        promotions.push(row)
-    })
-    .on('end', async rowCount => {
-        promotions.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
-        await dbOperation("deleteDocs", "promotion", [], {});
-        await dbOperation("insertDocs", "promotion", promotions);
-        const docs = await dbOperation("findDocs", "promotion", [], {"active": true}, {"_id": 1});
-        await console.log(docs);
-        await console.log(`Parsed ${rowCount} rows`);
-    });
-
-let countries = [];
-fs.createReadStream(path.resolve(__dirname, 'data', 'country.csv'))
-    .pipe(csv.parse({ headers: true }))
-    .on('error', error => console.error(error))
-    .on('data', row => {
-        countries.push(row)
-    })
-    .on('end', async rowCount => {
-        countries.sort((a, b) => a.code.localeCompare(b.code));
-        await dbOperation("deleteDocs", "country", [], {});
-        await dbOperation("insertDocs", "country", countries);
-        const docs = await dbOperation("findDocs", "country", [], {}, {"_id": 1});
-        await console.log(docs);
-        await console.log(`Parsed ${rowCount} rows`);
-    });
-
-const dbOperation = async (operation, collection, data, query, sort) => {
-    // for async it only works with Promise and resolve/reject
-    return new Promise(async (resolve, reject) => {
-        // Connect using the connection string
-        await MongoClient.connect(process.env['MONGODB_ATLAS_CLUSTER_URI'], { useNewUrlParser: true, useUnifiedTopology: true }, async (err, client) => {
-            if (err) {
-                reject(err);
-            } else {
-                //the following line is critical for performance reasons to allow re-use of database connections across calls to this Lambda function and avoid closing the database connection. The first call to this lambda function takes about 5 seconds to complete, while subsequent, close calls will only take a few hundred milliseconds.
-                var database = await client.db(process.env['DB_NAME']);
-                var response;
-                switch(operation) {
-                    case 'findDoc':
-                        response = await database.collection(collection).findOne(query);
-                        break;
-                    case 'findDocs':
-                        response = await database.collection(collection).find(query).sort(sort).toArray();
-                        break;
-                    case 'insertDoc':
-                        response = await database.collection(collection).insertOne(data);
-                        break;
-                    case 'insertDocs':
-                        response = await database.collection(collection).insertMany(data);
-                        break;
-                    case 'updateDoc':
-                        response = await database.collection(collection).updateOne(query, data);
-                        break;
-                    case 'udpateDocs':
-                        response = await database.collection(collection).updateMany(query, data);
-                        break;
-                    case 'deleteDoc':
-                        response = await database.collection(collection).deleteOne(query);
-                        break;
-                    case 'deleteDocs':
-                        response = await database.collection(collection).deleteMany(query);
-                        break;
-                    default:
-                        break;
+        promotionDeleteKeys.push({
+            DeleteRequest: {
+                Key: {
+                    "sequence": row.sequence
                 }
-                await client.close();
-                //await console.log(response);
-                resolve(response);
             }
         });
+        promotionItems.push({
+            PutRequest: {
+                Item: {
+                    "sequence": row.sequence,
+                    "title": row.title,
+                    "tagLine": row.tagLine,
+                    "image": row.image,
+                    "link": row.link,
+                    "active": row.active
+                }
+            }
+        });
+        promotionGetKeys.push({
+            "sequence": row.sequence
+        });
+
+    })
+    .on('end', async rowCount => {
+        await dbOperation("deleteDocs", "promotion", promotionDeleteKeys, {});
+        await dbOperation("insertDocs", "promotion", promotionItems);
+        const docs = await dbOperation("findDocs", "promotion", promotionGetKeys);
+        await console.log(docs);
+        await console.log(`Parsed ${rowCount} rows`);
     });
+
+let countryDeleteKeys = [];
+let countryItems = [];
+let countryGetKeys = [];
+fs.createReadStream(path.resolve(__dirname, 'data', 'country.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .transform(data => ({
+        code: data.code,
+        name: data.name
+    }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+        countryDeleteKeys.push({
+            DeleteRequest: {
+                Key: {
+                    "code": row.code,
+                    "name": row.name
+                }
+            }
+        });
+        countryItems.push({
+            PutRequest: {
+                Item: {
+                    "code": row.code,
+                    "name": row.name                }
+            }
+        });
+        countryGetKeys.push({
+            "code": row.code,
+            "name": row.name
+        });
+    })
+    .on('end', async rowCount => {
+        const batchSize = 25;
+        let len = countryItems.length;
+        for (let i = 0; i < len; i += batchSize) {
+            let tempCountryDeleteKeys = countryDeleteKeys.slice(i, i + batchSize);
+            await dbOperation("deleteDocs", "country", tempCountryDeleteKeys);
+            let tempCountryItems = countryItems.slice(i, i + batchSize);
+            await dbOperation("insertDocs", "country", tempCountryItems);
+            let tempCountryGetKeys = countryGetKeys.slice(i, i + batchSize);
+            const docs = await dbOperation("findDocs", "country", tempCountryGetKeys);
+            await console.log(docs);
+        }
+        await console.log(`Parsed ${rowCount} rows`);
+    });
+
+let termsAndConditionsKey = [];
+let termsAndConditionsItem = [];
+fs.readFile('data/termsAndConditions.json', 'utf8', async (err, data) => {
+    let termsAndConditionsList = await JSON.parse(data);
+    let docPromises = {};
+    docPromises = termsAndConditionsList.map(async (item) => {
+        //console.log(item);
+        termsAndConditionsKey = {
+            Key: {
+                "sequence": item.sequence
+            }
+        };
+        await dbOperation("deleteDoc", "termsAndConditions", termsAndConditionsKey);
+        termsAndConditionsItem = {
+            Item: {
+                "sequence": item.sequence,
+                "header": item.header,
+                "content": item.content
+            }
+        };
+        await dbOperation("insertDoc", "termsAndConditions", termsAndConditionsItem);
+        const doc = await dbOperation("findDoc", "termsAndConditions", termsAndConditionsKey);
+        return doc;
+    });
+    var docs = await Promise.all(docPromises);
+    console.log(docs);
+});
+
+let privacyPolicyKey = {};
+let privacyPolicyItem = {};
+fs.readFile('data/privacyPolicy.json', 'utf8', async (err, data) => {
+    let privacyPolicyList = await JSON.parse(data);
+    let docPromises = {};
+    docPromises = privacyPolicyList.map(async (item) => {
+        privacyPolicyKey = {
+            Key: {
+                "sequence": item.sequence
+            }
+        };
+        await dbOperation("deleteDoc", "privacyPolicy", privacyPolicyKey);
+        privacyPolicyItem = {
+            Item: {
+                "sequence": item.sequence,
+                "header": item.header,
+                "content": item.content
+            }
+        };
+        await dbOperation("insertDoc", "privacyPolicy", privacyPolicyItem);
+        const doc = await dbOperation("findDoc", "privacyPolicy", privacyPolicyKey);
+        return doc;
+    });
+    const docs = await Promise.all(docPromises);
+    console.log(docs);
+});
+
+let frequentlyAskedQuestionKey = {};
+let frequentlyAskedQuestionItem = {};
+fs.readFile('data/frequentlyAskedQuestion.json', 'utf8', async (err, data) => {
+    let frequentlyAskedQuestionList = await JSON.parse(data);
+    let docPromises = {};
+    docPromises = frequentlyAskedQuestionList.map(async (item) => {
+        frequentlyAskedQuestionKey = {
+            Key: {
+                "sequence": item.sequence
+            }
+        };
+        await dbOperation("deleteDoc", "frequentlyAskedQuestion", frequentlyAskedQuestionKey);
+        frequentlyAskedQuestionItem = {
+            Item: {
+                "sequence": item.sequence,
+                "section": item.section,
+                "questionAndAnswerList": item.questionAndAnswerList
+            }
+        }
+        await dbOperation("insertDoc", "frequentlyAskedQuestion", frequentlyAskedQuestionItem);
+        const doc = await dbOperation("findDoc", "frequentlyAskedQuestion", frequentlyAskedQuestionKey);
+        return doc;
+    });
+    const docs = await Promise.all(docPromises);
+    console.log(docs);
+});
+
+/*let auditItem = {};
+fs.readFile('data/audit.json', 'utf8', async (err, data) => {
+    let auditList = await JSON.parse(data);
+    let docPromises = {};
+    docPromises = auditList.map(async (item) => {
+        auditItem = {
+            Item: {
+                "date": item.date,
+                "hostName": item.hostName,
+                "countryCode": item.countryCode,
+                "ipAddress": item.ipAddress,
+                "page": item.page,
+                "message": item.message
+            }
+        }
+        const doc = await dbOperation("insertDoc", "audit", auditItem);
+        return doc;
+    });
+    const docs = await Promise.all(docPromises);
+    console.log(docs);
+});*/
+
+const dbOperation = async (operation, table, data, filter) => {
+    var tableName = process.env['ENVIRONMENT'] + '.' + process.env['APP_NAME'] + '.' + process.env['SERVICE_NAME'] + '.' + table;
+    var response;
+    var params;
+    try {
+        switch(operation) {
+            case 'findDoc':
+                data.TableName = tableName;
+                params = data;
+                response = await database.get(params);
+                break;
+            case 'findDocs':
+                params = {
+                    "RequestItems": {
+                        [tableName]: {
+                            "Keys": data
+                        }
+                    }
+                }
+                response = await database.batchGet(params, tableName);
+                break;
+            case 'insertDoc':
+                data.TableName = tableName;
+                params = data;
+                response = await database.put(params);
+                break;
+            case 'insertDocs':
+                params = {
+                    "RequestItems": {
+                        [tableName]: data
+                    }
+                }
+                response = await database.batchWrite(params);
+                break;
+            case 'updateDoc':
+                //response = await database.collection(collection).updateOne(query, data);
+                break;
+            case 'udpateDocs':
+                //response = await database.collection(collection).updateMany(query, data);
+                break;
+            case 'deleteDoc':
+                data.TableName = tableName;
+                params = data;
+                response = await database.delete(params);
+                break;
+            case 'deleteDocs':
+                params = {
+                    "RequestItems": {
+                        [tableName]: data
+                    }
+                }
+                response = await database.batchWrite(params);
+                break;
+            case 'queryDocs':
+                filter.TableName = tableName
+                params = filter;
+                response = await database.query(params);
+                break;
+            case 'scanDocs':
+                filter.TableName = tableName
+                params = filter;
+                response = await database.scan(params);
+                break;
+            case 'putDoc':
+                data.TableName = tableName
+                params = data;
+                response = await database.putDocument(params);
+                break;
+            default:
+                break;
+        }
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+
 }
--- core-service/handler.js
+++ core-service/handler.js
@@ -1,14 +1,22 @@
 'use strict';
-const db = require('./db');
-const collection = process.env['COLLECTION_NAME'];
+const database = require('./db');
+const table = process.env['ENVIRONMENT'] + '.' + process.env['APP_NAME'] + '.' + process.env['SERVICE_NAME'] + '.' + process.env['TABLE_NAME'];
 
 module.exports.findMenuList = async (event) => {
     let active  = (event.pathParameters.active === 'true');
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {"active" : active}, {"sequence": 1});
+    const params = {
+        TableName: table,
+        FilterExpression: '#active = :active_val',
+        ExpressionAttributeNames: {
+            '#active': 'active',
+        },
+        ExpressionAttributeValues: {':active_val': active}
+    };
+    const menus = await database.scan(params);
+    menus.sort((a,b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(menus),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -18,11 +26,19 @@ module.exports.findMenuList = async (event) => {
 
 module.exports.findPromotionList = async (event) => {
     let active  = (event.pathParameters.active === 'true');
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {"active" : active}, {"sequence": 1});
+    const params = {
+        TableName: table,
+        FilterExpression: '#active = :active_val',
+        ExpressionAttributeNames: {
+            '#active': 'active',
+        },
+        ExpressionAttributeValues: {':active_val': active}
+    };
+    const promotions = await database.scan(params);
+    promotions.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(promotions),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -31,11 +47,14 @@ module.exports.findPromotionList = async (event) => {
 };
 
 module.exports.findAboutUsList = async (event) => {
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {}, {"sequence": 1});
+    const params = {
+        TableName: table
+    };
+    const aboutUsList = await database.scan(params);
+    aboutUsList.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(aboutUsList),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -44,11 +63,14 @@ module.exports.findAboutUsList = async (event) => {
 };
 
 module.exports.findTermsAndConditionsList = async (event) => {
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {}, {"_id": 1});
+    const params = {
+        TableName: table
+    };
+    const termsAndConditionsList = await database.scan(params);
+    termsAndConditionsList.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(termsAndConditionsList),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -57,11 +79,14 @@ module.exports.findTermsAndConditionsList = async (event) => {
 };
 
 module.exports.findPrivacyPolicyList = async (event) => {
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {}, {"_id": 1});
+    const params = {
+        TableName: table
+    };
+    const privacyPolicyList = await database.scan(params);
+    privacyPolicyList.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(privacyPolicyList),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -70,11 +95,14 @@ module.exports.findPrivacyPolicyList = async (event) => {
 };
 
 module.exports.findFrequentlyAskedQuestionList = async (event) => {
-    const database = await db.get();
-    const docs = await db.findDocuments(database, collection, {}, {"_id": 1, "questionAndAnswerList.sequence": 1});
+    const params = {
+        TableName: table
+    };
+    const frequentlyAskedQuestionList = await database.scan(params);
+    frequentlyAskedQuestionList.sort((a, b) => (a.sequence > b.sequence) ? 1 : ((b.sequence > a.sequence) ? -1 : 0));
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(frequentlyAskedQuestionList),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -84,11 +112,19 @@ module.exports.findFrequentlyAskedQuestionList = async (event) => {
 
 module.exports.findCountryByCode = async (event) => {
     let countryCode  = event.pathParameters.countryCode;
-    const database = await db.get();
-    const docs = await db.findDocument(database, collection, {"code" : countryCode});
+    const params = {
+        TableName: table,
+        KeyConditionExpression: '#code = :code',
+        ExpressionAttributeNames: {
+            '#code': 'code',
+        },
+        ExpressionAttributeValues: {':code': countryCode}
+    };
+    const country = await database.query(params);
+
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(country),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -98,11 +134,18 @@ module.exports.findCountryByCode = async (event) => {
 
 module.exports.findCountryByName = async (event) => {
     let countryName  = event.pathParameters.countryName;
-    const database = await db.get();
-    const docs = await db.findDocument(database, collection, {"name" : countryName});
+    const params = {
+        TableName: table,
+        FilterExpression: '#name = :name',
+        ExpressionAttributeNames: {
+            '#name': 'name',
+        },
+        ExpressionAttributeValues: {':name': countryName}
+    };
+    const country = await database.scan(params);
     return {
         statusCode: 200,
-        body: JSON.stringify(docs),
+        body: JSON.stringify(country),
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
@@ -112,8 +155,18 @@ module.exports.findCountryByName = async (event) => {
 
 module.exports.createAuditEntry = async (event) => {
     const data = JSON.parse(event.body);
-    const database = await db.get();
-    const doc = await db.insertDocument(database, collection, data);
+    const params = {
+        TableName: table,
+        Item: {
+            "date": data.date,
+            "hostName": data.hostName,
+            "countryCode": data.countryCode,
+            "ipAddress": data.ipAddress,
+            "page": data.page,
+            "message": data.message
+        }
+    }
+    const doc = await database.put(params);
     return {
         statusCode: 200,
         body: JSON.stringify(doc),
